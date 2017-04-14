040c448de6f818ac971d37b012809578cd04dea5
Bryan Goldstein <brysgo@gmail.com>
2017-04-14 01:28:03 +0000

    Implement simpler container API
    * First stab at #3

--- react/index.js
+++ react/index.js
@@ -1,49 +1,58 @@
-const graphqlGun = require("../");
+const graphqlGunUtil = require("../");
 
 module.exports = function({ React, gun }) {
-  return {
-    createContainer: function createContainer(Component, { fragments }) {
-      class Container extends React.Component {
-        constructor() {
-          super();
-          this.state = {};
-          this.resetPromise();
-        }
-
-        resetPromise() {
-          let resolve;
-          this.promise = new Promise(res => resolve = res);
-          if (this.resolve) this.resolve(() => this.promise);
-          this.resolve = resolve;
-        }
-
-        reloadFragmentOnResolve(fragment, asyncIterator) {
-          return asyncIterator.next().then(iter => {
-            this.setState({ data: { [fragment]: iter.value } });
-            this.resetPromise(); // for testing
-            this.reloadFragmentOnResolve(fragment, iter);
-          });
-        }
-
-        componentDidMount() {
-          return Object.keys(fragments).map(fragment => {
-            const asyncIterator = graphqlGun(fragments[fragment], gun)[
-              Symbol.iterator
-            ]();
-            this.reloadFragmentOnResolve(fragment, asyncIterator);
-            return asyncIterator;
-          });
-        }
-
-        render() {
-          return React.createElement(
-            Component,
-            Object.assign({}, this.state.data, this.props)
-          );
-        }
+  function createContainer(Component, { fragments }) {
+    class Container extends React.Component {
+      constructor() {
+        super();
+        this.state = {};
+        this.resetPromise();
       }
-      Container.displayName = `${Component.displayName}DataContainer`;
-      return Container;
+
+      resetPromise() {
+        let resolve;
+        this.promise = new Promise(res => resolve = res);
+        if (this.resolve) this.resolve(() => this.promise);
+        this.resolve = resolve;
+      }
+
+      reloadFragmentOnResolve(fragment, asyncIterator) {
+        return asyncIterator.next().then(iter => {
+          this.setState({ data: { [fragment]: iter.value } });
+          this.resetPromise(); // for testing
+          this.reloadFragmentOnResolve(fragment, iter);
+        });
+      }
+
+      componentDidMount() {
+        return Object.keys(fragments).map(fragment => {
+          const asyncIterator = graphqlGunUtil(fragments[fragment], gun)[
+            Symbol.iterator
+          ]();
+          this.reloadFragmentOnResolve(fragment, asyncIterator);
+          return asyncIterator;
+        });
+      }
+
+      render() {
+        return React.createElement(
+          Component,
+          Object.assign({}, this.state.data, this.props)
+        );
+      }
     }
+    Container.displayName = `${Component.displayName}DataContainer`;
+    return Container;
+  }
+  
+  function graphqlGun(data) {
+    return function wrap(Component) {
+      return createContainer(Component, { fragments: {data} });
+    };
+  }
+
+  return {
+    createContainer,
+    graphqlGun
   };
 };
--- react/index.test.js
+++ react/index.test.js
@@ -1,90 +1,120 @@
 /* global describe, it, expect */
 const gql = require("graphql-tag");
 const Gun = require("gun/gun");
-const graphqlGun = require("../");
 const React = require("react");
 const gun = Gun();
-const { createContainer } = require("./")({ React, gun });
+const { createContainer, graphqlGun } = require("./")({ React, gun });
 
 const renderer = require("react-test-renderer");
 
-const Color = ({ color, palette }) => {
+const Color = (props) => {
   return React.createElement(
     "div",
-    { style: { color } },
-    JSON.stringify(palette, null, 2)
+    { style: { color: props.color } },
+    JSON.stringify(props, null, 2)
   );
 };
 
-describe("createContainer", () => {
-  it("renders the fragment in the container", async () => {
-    gun
-      .get("color")
-      .put({ hue: 255, saturation: 255, value: 255, other: "foo" });
-
-    const ColorContainer = createContainer(Color, {
-      fragments: {
-        palette: gql`{
-          color {
-            hue
-            saturation
-            value
-          }
-        }`
-      }
-    });
-
-    let containerInstance;
-    const colorContainerElement = React.createElement(ColorContainer, {
-      color: "blue",
-      ref: container => containerInstance = container
-    });
-    const component = renderer.create(colorContainerElement);
-
-    await containerInstance.promise;
-
-    let tree = component.toJSON();
-    expect(tree).toMatchSnapshot();
-  });
-
-  it("reloads your component when new data comes", async () => {
-    gun.get("thing").put({
-      stuff: {
-        one: "hello",
-        two: "world"
-      }
-    });
-
-    const ColorContainer = createContainer(Color, {
-      fragments: {
-        palette: gql`{
-          thing {
-            stuff @live {
-              one
-              two
+describe("react", () => {
+  describe("createContainer", () => {
+    it("renders the fragment in the container", async () => {
+      gun
+        .get("color")
+        .put({ hue: 255, saturation: 255, value: 255, other: "foo" });
+  
+      const ColorContainer = createContainer(Color, {
+        fragments: {
+          palette: gql`{
+            color {
+              hue
+              saturation
+              value
             }
-          }
-        }`
-      }
+          }`
+        }
+      });
+  
+      let containerInstance;
+      const colorContainerElement = React.createElement(ColorContainer, {
+        color: "blue",
+        ref: container => containerInstance = container
+      });
+      const component = renderer.create(colorContainerElement);
+  
+      await containerInstance.promise;
+  
+      let tree = component.toJSON();
+      expect(tree).toMatchSnapshot();
     });
-
-    let containerInstance;
-    const colorContainerElement = React.createElement(ColorContainer, {
-      color: "red",
-      ref: container => containerInstance = container
+  
+    it("reloads your component when new data comes", async () => {
+      gun.get("thing").put({
+        stuff: {
+          one: "hello",
+          two: "world"
+        }
+      });
+  
+      const ColorContainer = createContainer(Color, {
+        fragments: {
+          palette: gql`{
+            thing {
+              stuff @live {
+                one
+                two
+              }
+            }
+          }`
+        }
+      });
+  
+      let containerInstance;
+      const colorContainerElement = React.createElement(ColorContainer, {
+        color: "red",
+        ref: container => containerInstance = container
+      });
+      const component = renderer.create(colorContainerElement);
+  
+      const next = await containerInstance.promise;
+  
+      let tree = component.toJSON();
+      expect(tree).toMatchSnapshot();
+  
+      gun.get("thing").put({ stuff: { one: "new" } });
+  
+      await next();
+  
+      tree = component.toJSON();
+      expect(tree).toMatchSnapshot();
     });
-    const component = renderer.create(colorContainerElement);
-
-    const next = await containerInstance.promise;
-
-    let tree = component.toJSON();
-    expect(tree).toMatchSnapshot();
-
-    gun.get("thing").put({ stuff: { one: "new" } });
-
-    await next();
-
-    tree = component.toJSON();
-    expect(tree).toMatchSnapshot();
+  });
+  
+  describe("graphqlGun", () => {
+    it("provides apollo client syntax for container", async () => {
+      gun
+        .get("color")
+        .put({ hue: 255, saturation: 255, value: 255, other: "foo" });
+  
+      const ColorContainer = graphqlGun(gql`{
+            color {
+              hue
+              saturation
+              value
+            }
+          }`)(Color);
+  
+      let containerInstance;
+      const colorContainerElement = React.createElement(ColorContainer, {
+        color: "blue",
+        ref: container => containerInstance = container
+      });
+      const component = renderer.create(colorContainerElement);
+  
+      await containerInstance.promise;
+  
+      let tree = component.toJSON();
+      expect(tree).toMatchSnapshot();
+    });
+  
   });
 });
